322b5a39a04f13715dc9722168cb353e6288e16d
Charlie Wray <charlesmwray@gmail.com>
2017-07-25 17:35:39 -0700

    Upgrades firebase version and initialization

--- src/Data/firebase.js
+++ src/Data/firebase.js
@@ -1,4 +1,15 @@
 /*eslint no-undef: 0*/
-const firebase = new Firebase('https://sweltering-fire-733.firebaseio.com/movies/');
+var config = {
+    apiKey: "AIzaSyBmdypyXBSh6_Pble2-3o8alfVogwi3yIs",
+    authDomain: "sweltering-fire-733.firebaseapp.com",
+    databaseURL: "https://sweltering-fire-733.firebaseio.com/",
+    projectId: "sweltering-fire-733",
+    storageBucket: "sweltering-fire-733.appspot.com",
+    messagingSenderId: "379353179305"
+};
 
-export default firebase;
+firebase.initializeApp(config);
+
+const data = firebase.database().ref('movies')
+
+export default data;
